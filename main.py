dde111353d2118fdc442cb92ec79b0edd6059175
veb-bet <73333734+veb-bet@users.noreply.github.com>
2022-07-09 11:48:06 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-
+import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+from PyQt5.QtGui     import *
+from PyQt5.QtCore    import *
+from PyQt5.QtWidgets import *
 
 
 class Ui_MainWindow(QWidget):
@@ -18,15 +20,27 @@ class Ui_MainWindow(QWidget):
 
         self.textEdit = QtWidgets.QTextEdit(self)
         self.textEdit.setGeometry(QtCore.QRect(30, 20, 741, 251))
-        
-        self.pushButton = QtWidgets.QPushButton("Открыть из файла", self)
-        self.pushButton.setGeometry(QtCore.QRect(462, 310, 311, 28))
 
-        self.pushButton_2 = QtWidgets.QPushButton("Анализировать эмоции", self)
-        self.pushButton_2.setGeometry(QtCore.QRect(460, 400, 311, 28))
+        self.openFileButton = QtWidgets.QPushButton("Открыть файл", self)
+        self.openFileButton.setGeometry(QtCore.QRect(462, 310, 311, 28))
+        self.openFileButton.clicked.connect(self.getFileName)
 
-        self.pushButton_3 = QtWidgets.QPushButton("Анализировать психологические расстройства", self)
-        self.pushButton_3.setGeometry(QtCore.QRect(460, 490, 311, 28))
+        self.pushButton_2 = QtWidgets.QPushButton("Анализ текста", self)
+        self.pushButton_2.setGeometry(QtCore.QRect(460, 490, 311, 28))
+
+        self.openfile = ""
+
+    def getFileName(self):
+        dirlist = QFileDialog.getOpenFileName(self)
+        self.openfile = dirlist[0]
+        print(self.openfile)
+        file = open(self.openfile)
+        s = ""
+        for line in file:
+            s += line
+        file.close()
+        print(s)
+        self.textEdit.setText(s)
 
 if __name__ == "__main__":
     import sys
